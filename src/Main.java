9abf9c56955220783ad608a155a9475adec0edc7
Viktor Tsarkov <mcgrowsr@gmail.com>
2025-04-15 11:50:18 +0300

    Домашная работа, урок 4

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,70 @@
 public class Main {
 
-    public static void main(String[] args) {
-        printHello();
-        System.out.println(hello("Elena"));
+    // Задание 1. Создайте метод sumRange, который принимает два числа (начало и конец диапазона)
+    // и возвращает сумму всех чисел между ними (включительно)
+    public static int sumRange(int first, int last) {
+        int sum = 0;
 
-        System.out.printf("%s * %s = %s%s", 2, 3, multiply(2, 3), System.lineSeparator());
-
-        int factorial = factorial(3);
-        System.out.println(factorial);
-    }
-
-    /**
-     * (модификатор доступа)
-     * (static - вызов метода возможен без создания обьекта)
-     * (возвращаемый типа или void)
-     * (имя метода)
-     * (входящие параметры)
-     */
-    public static void printHello() {
-        System.out.println("Hello");
-    }
-
-    public static String hello(String name) {
-        return "Hello " + name;
-    }
-
-    public static int multiply(int a, int b) {
-        return a * b;
-    }
-
-    /**
-     * Одниковые названия методов, но разные типы данных (входящие) или количество параметров
-     */
-    public static double multiply(double a, double b) {
-        return a * b;
-    }
-
-    public static int multiply(int a, int b, int c) {
-        return a * b * c;
-    }
-
-    /**
-     * Рекурсия
-     * Факториал 3! = 1 * 2 * 3 = 6
-     */
-    static int factorial(int n) {
-        if (n <= 1) {
-            return 1;
+        while (first <= last) {
+            sum += first;
+            first++;
         }
-        return n * factorial(n - 1);
+
+        return sum;
     }
 
-    /**
-     * factorial(3)
-     *     3 * factorial(2)
-     *          2 * factorial(1)
-     *               1 (базовый случай)
-     *          -> 2 * 1 = 2
-     *     -> 3 * 2
-     * -> 6
-     */
+    // Задание 2. Создайте два перегруженных метода add: Первый складывает два целых числа;
+    // Второй складывает две строки
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static String add(String a, String b) {
+        return a + b;
+    }
+
+    // Задание 3. Напишите метод findMax, который принимает массив целых чисел и возвращает его максимальный элемент
+    public static int findMax(int[] list) {
+        int numberMax = list[0];
+
+        for (int i = 1; i < list.length; i++) {
+            if (list[i] > numberMax) {
+                numberMax = list[i];
+            }
+        }
+
+        return numberMax;
+    }
+
+    // Задание 4. Напишите метод, вычисляющий факториал числа с помощью цикла for
+    public static int calculateFactorial(int f) {
+        int result = 1;
+
+        for (int i = 1; i <= f ; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    // Задание 5. Создайте перегруженные методы calculateArea: Для круга (принимает радиус);
+    // Для прямоугольника (принимает длину и ширину)
+    public static double calculateArea(int radius) {
+        return 3.14 * radius * radius;
+    }
+
+    public static int calculateArea(int a, int b) {
+        return a * b;
+    }
+
+    // Задание 6. Реализуйте метод, который принимает три числа и возвращает их среднее арифметическое
+    public static int calculateAverage(int a, int b, int c) {
+        return (a + b + c) / 3;
+    }
+
+    // Задание 7. Напишите метод, который принимает два числа, представляющие длины катетов,
+    // и возвращает длину гипотенузы (используйте теорему Пифагора)
+    public static double calculateHypotenuse(int a, int b) {
+        return Math.sqrt(a * a + b * b);
+    }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -6,14 +6,63 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void hello() {
-        String name = "Котик";
-        Assertions.assertEquals("Hello " + name, Main.hello(name));
+    void sumRange() {
+        int a = 4;
+        int b = 7;
+        Assertions.assertEquals(22, Main.sumRange(a, b));
     }
 
     @Test
-    void multiply() {
-        int a = 2, b = 3;
-        Assertions.assertEquals(a * b, Main.multiply(a, b));
+    void add() {
+        int a = 10;
+        int b = 3;
+        Assertions.assertEquals(a + b, Main.add(a, b));
+    }
+
+    @Test
+    void testAdd() {
+        String a = "Привет,";
+        String b = " Мир!";
+        Assertions.assertEquals(a + b, Main.add(a, b));
+    }
+
+    @Test
+    void findMax() {
+        int[] list = {2, 35, 0, -5, 6, 78, 11, 12, 99};
+        Assertions.assertEquals(99, Main.findMax(list));
+    }
+
+    @Test
+    void calculateFactorial() {
+        int a = 5;
+        Assertions.assertEquals(120, Main.calculateFactorial(a));
+    }
+
+    @Test
+    void calculateArea() {
+        int a = 5;
+        Assertions.assertEquals(3.14 * a * a, Main.calculateArea(a));
+    }
+
+    @Test
+    void testCalculateArea() {
+        int a = 7;
+        int b = 10;
+        Assertions.assertEquals(a * b, Main.calculateArea(a, b));
+    }
+
+    @Test
+    void calculateAverage() {
+        int a = 4;
+        int b = 7;
+        int c = 25;
+        Assertions.assertEquals((a + b + c) / 3, Main.calculateAverage(a, b, c));
+    }
+
+    @Test
+    void calculateHypotenuse() {
+        int a = 8;
+        int b = 6;
+        Assertions.assertEquals(10, Main.calculateHypotenuse(a, b));
     }
 }
